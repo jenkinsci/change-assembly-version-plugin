713c1dd431568aeff34840025045001ac890fdbe
Wookhyun Shin <wshin@satreci.com>
2014-11-20 12:39:46 +0900

    Add support complex version number.
    Add support to use static text and environment variable in version
    field.

--- src/main/java/org/jenkinsci/plugins/changeassemblyversion/AssemblyVersion.java
+++ src/main/java/org/jenkinsci/plugins/changeassemblyversion/AssemblyVersion.java
@@ -5,37 +5,56 @@ import hudson.model.BuildListener;
 
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import hudson.model.BuildListener;
 
 public class AssemblyVersion {
 	
 	
 	private String version;
 	private EnvVars envVars;	
+	private BuildListener listener;
 	
 	/**
 	 * The instance of this class gonna return in the property version the value to be used on ChangeTools.
 	 * @param version
 	 * @param envVars
 	 */
-	public AssemblyVersion(String version, EnvVars envVars){		
+	public AssemblyVersion(String version, EnvVars envVars, BuildListener listener){		
 		this.envVars = envVars;
 		this.version = version;		
-		if(version.contains("${")){					
-			this.version = removeSyntax();			
-			this.version = getEnvVersion();			
+		this.listener = listener;
+		
+		if(version.contains("${")){
+			listener.getLogger().println("Version contains variable.");
+			this.version = getEnvVersion(version);			
 		}
-	}
-	
-	public void setVersion(String version){
-		this.version = version;
 	}
 	
 	public String getVersion(){
 		return this.version;
 	}
 	
-	private String getEnvVersion(){
-		return envVars.get(this.version);
+	private String getEnvVersion(String version){
+		String pattern = "\\$\\{(.+)\\}";
+		Pattern p = Pattern.compile(pattern);
+	    Matcher m = p.matcher(version);	
+
+		String result = version;		
+	    if(m.find()){
+			String variable = m.group(1);			
+			listener.getLogger().println(variable);			
+			listener.getLogger().println("->");
+			
+			String value = envVars.get(variable);			
+			listener.getLogger().println(value);
+			result = m.replaceFirst(value);
+	    }
+		else
+		{
+			listener.getLogger().println("No variable is replaced");
+		}
+		
+		return result;
 	}
 	
 	private String removeSyntax(){
