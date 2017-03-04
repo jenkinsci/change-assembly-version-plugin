ecd8286de658e2e6571e702a6000e287630e80cd
Michael DK Fowler <michaeldkfowler@gmail.com>
2017-03-04 17:57:42 -0500

    Fix inserting duplicate properties

--- src/main/java/org/jenkinsci/plugins/changeassemblyversion/ChangeTools.java
+++ src/main/java/org/jenkinsci/plugins/changeassemblyversion/ChangeTools.java
@@ -2,6 +2,7 @@ package org.jenkinsci.plugins.changeassemblyversion;
 
 import hudson.model.BuildListener;
 import java.io.IOException;
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class ChangeTools {
@@ -14,10 +15,11 @@ public class ChangeTools {
             //listener.getLogger().println(String.format("\t Replacement : %s", replacement));
             //String newContent = content.replaceAll(regexPattern.toString(), String.format(replacementPattern, replacement));
             //regexPattern.matcher(content).region(0, 0);
-            content = regexPattern.matcher(content).replaceFirst(String.format(replacementPattern, replacement));
+            Matcher m=regexPattern.matcher(content);
+            content = m.replaceFirst(String.format(replacementPattern, replacement));
             //listener.getLogger().println(String.format("regex= %s",regexPattern.matcher(content).pattern()));
             try {
-                regexPattern.matcher(content).group(); //throws illegalstate if no match was perfomred
+                m.group(); //throws illegalstate if no match was perfomred
             } catch (IllegalStateException ex) {
                 listener.getLogger().println("Addidng missing value");
                 content += System.lineSeparator() + String.format(replacementPattern, replacement);
