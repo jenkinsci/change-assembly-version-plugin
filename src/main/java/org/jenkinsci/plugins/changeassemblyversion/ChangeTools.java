3d39d851039e481077e03117ab7280ab34c39673
kevenli <pbleester@gmail.com>
2019-04-26 14:00:05 +0800

    Fix bug https://issues.jenkins-ci.org/browse/JENKINS-36375, support BOM encoding of AssemblyInfo.cs file

--- src/main/java/org/jenkinsci/plugins/changeassemblyversion/ChangeTools.java
+++ src/main/java/org/jenkinsci/plugins/changeassemblyversion/ChangeTools.java
@@ -1,11 +1,14 @@
 package org.jenkinsci.plugins.changeassemblyversion;
 
+import com.sun.media.jfxmedia.track.Track;
 import hudson.FilePath;
 import hudson.model.BuildListener;
 import hudson.model.TaskListener;
+import org.apache.commons.io.ByteOrderMark;
+import org.apache.commons.io.input.BOMInputStream;
 
-import java.io.OutputStream;
-import java.io.IOException;
+import java.io.*;
+import java.nio.charset.Charset;
 
 public class ChangeTools {
 
@@ -31,13 +34,33 @@ public class ChangeTools {
     public void Replace(String replacement, TaskListener listener) throws IOException, InterruptedException {
         if (replacement != null && !replacement.isEmpty())
         {
-            String content = file.readToString();  // needs to use read() instead!
+            BOMInputStream inputStream = new BOMInputStream(file.read());
+            ByteOrderMark bom = inputStream.getBOM();
+
+            Charset fileEncoding = Charset.defaultCharset();
+            if(inputStream.hasBOM()) {
+                fileEncoding = Charset.forName(inputStream.getBOM().getCharsetName());
+            }
+            byte[] buffer = new byte[8192];
+            ByteArrayOutputStream bs = new ByteArrayOutputStream();
+            BufferedOutputStream out = new BufferedOutputStream(bs);
+            int read = inputStream.read(buffer, 0, buffer.length);
+            while (read > 0){
+                out.write(buffer, 0, read);
+                read = inputStream.read(buffer, 0, buffer.length);
+            }
+            out.flush();
+            String content = new String(bs.toByteArray(), fileEncoding);
             listener.getLogger().println(String.format("Updating file : %s, Replacement : %s", file.getRemote(), replacement));
             content = content.replaceAll(regexPattern, String.format(replacementPattern, replacement));
             //listener.getLogger().println(String.format("Updating file : %s", file.getRemote()));
             OutputStream os = file.write();
             try {
-                os.write(content.getBytes());
+                if (inputStream.hasBOM())
+                {
+                    os.write(inputStream.getBOM().getBytes());
+                }
+                os.write(content.getBytes(fileEncoding));
             } finally {
                 os.close();
             }
